2a10344f41cffae5255b7b20b878c787969a93f8
juan lucas murcia solano <jlmurcia@uninorte.edu.co>
2024-11-01 18:16:57 -0500

    registro en git config

---
+++ src/proyecto_informacion_estudiantes/PROYECTO_INFORMACION_ESTUDIANTES.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_informacion_estudiantes;
+
+/**
+ *
+ * @author jsgallardo
+ */
+public class PROYECTO_INFORMACION_ESTUDIANTES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        InformacionAlummnos frame = new InformacionAlummnos();
+        frame.setVisible(true);
+        frame.setSize(650, 450);
+    }
+
+}
